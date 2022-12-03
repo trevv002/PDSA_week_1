792ccf9e5a50f8a67d39f2baed3d00654f40303c
trevv002 <99491727+trevv002@users.noreply.github.com>
2022-12-03 13:36:33 +0530

    Add files via upload

---
+++ finding_odd_elem.py
@@ -0,0 +1,12 @@
+def odd_one(L):
+  P={}
+  for elem in L:
+    if type(elem )not in P:
+       P[type(elem)] == 0:
+    P[type(elem)]+=1
+  for key, value in P.items():
+    if value==1:
+      return key.__name__
+      
+  
+      
