2b69bad91a96c5110ba76f02dbbb02eca4c5b0b0
trevv002 <99491727+trevv002@users.noreply.github.com>
2022-12-03 13:15:27 +0530

    Add files via upload

---
+++ Find_min_diff.py
@@ -0,0 +1,9 @@
+def Find_min_difference(L,P):
+  L.sort()
+  N=P
+  M=len(L)
+  min_diff=max(L) - min(L)
+  for i in range(M-N+1):
+    if L[i+N-1] - L[i] < min_diff:
+      min_diff = L[i+N-1] - L[i]
+  return min_diff
